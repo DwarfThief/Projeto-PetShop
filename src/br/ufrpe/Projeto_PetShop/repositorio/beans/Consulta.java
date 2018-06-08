ae01a4a66929c2730d0871e296234b88ac828ed1
Brenno Barboza <brennol.c.b@hotmail.com>
2018-06-08 15:46:45 -0300

    Conserto básico no Consulta.java

--- src/br/ufrpe/Projeto_PetShop/repositorio/beans/Consulta.java
+++ src/br/ufrpe/Projeto_PetShop/repositorio/beans/Consulta.java
@@ -1,31 +1,35 @@
 package br.ufrpe.Projeto_PetShop.repositorio.beans;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Consulta {
 	private Animal animal;
 	private Veterinario veterinario;
-	private LocalDate time;
+	private LocalDateTime data;
 	
+	public Consulta(Animal nome, Veterinario medico) {
+		this.animal=nome;
+		this.veterinario = medico;
+		this.data = LocalDateTime.now();
+	}
+	public void setAnimal(Animal animal) {
+		this.animal = animal;
+	}
 	public Animal getAnimal() {
 		return animal;
 	}
-	
+	public void setVeterinario(Veterinario veterinario) {
+		this.veterinario = veterinario;
+	}
 	public Veterinario getVeterinario() {
 		return veterinario;
 	}
+	public LocalDateTime getTime() {
+		return data;
+	}	
 	
-	public LocalDate getTime() {
-		return time;
-	}
-	
-	public void consulta(Animal nome, Veterinario medico) {
-		this.animal=nome;
-		this.veterinario = medico;
-		this.time = LocalDate.now();
-	}
 	
 	public String toString() {
-		return this.animal.getNome() + " foi consultado por " + this.veterinario.getNome() + " as " + time;
+		return this.animal.getNome() + " foi consultado por " + this.veterinario.getNome() + " as " + data;
 	}
 }
