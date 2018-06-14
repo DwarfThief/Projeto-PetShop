7ee7eb0adcd19c90e582def7e3a5ec8c222e37d9
Geoleane <gbenjamim12@gmail.com>
2018-06-14 13:57:46 -0300

    Mudanças nos Repositorios

--- src/br/ufrpe/Projeto_PetShop/repositorio/RepositorioCliente.java
+++ src/br/ufrpe/Projeto_PetShop/repositorio/RepositorioCliente.java
@@ -6,8 +6,7 @@ public class RepositorioCliente {
 	private Cliente clientes[]=new Cliente[5];
 	private int clientesTam=0;
 	
-	public RepositorioCliente () {}
-	
+	public RepositorioCliente () {}	
 	private Cliente procurarCliente (String cpf) {
 		for(int i=0;i<clientesTam;i++) {
 			if(clientes[i].getCpf().equals(cpf)) {
@@ -15,24 +14,43 @@ public class RepositorioCliente {
 			}
 		}
 		return null;
-	}
-	
-	public void adicionarCliente(Cliente cliente) {
+	}	
+	private int procurarPos(String cpf) {
+		int i = 0;
+        for(; i<this.clientesTam; i++) {
+        	if(cpf.equals(this.clientes[i].getCpf())) {
+        		return i;
+        	}
+        }
+        return -1;
+	}	
+	public void addCliente(Cliente cliente) {
+		if(this.clientesTam == this.clientes.length) {
+			this.duplicaArray();
+		}
 		this.clientes[clientesTam]=cliente;
-		clientesTam++;
-	}
-	
+		this.clientesTam++;
+	}	
 	public Cliente getCliente(String cpf) {
 		return this.procurarCliente(cpf);
-	}
-	
-	public void removeFuncionario(String cpf) {
-		for(int i=0;i<clientesTam;i++) {
-			if(clientes[i].getCpf().equals(cpf)) {
-				this.clientes[i]=this.clientes[clientesTam-1];
-				this.clientes[clientesTam-1]=null;
-				clientesTam--;
-			}
-		}
-	}
+	}	
+	public void removeCliente(String cpf) {
+		int i = this.procurarPos(cpf);
+		if (i != this.clientesTam) {
+            this.clientes[i] = this.clientes[this.clientesTam - 1];
+            this.clientes[this.clientesTam - 1] = null;
+            this.clientesTam = this.clientesTam - 1;
+        } else {
+            //falta implementar exception
+        }
+	}	
+	private void duplicaArray() {
+        if (this.clientes != null && this.clientes.length > 0) {
+            Cliente[] arrayDuplicado = new Cliente[this.clientes.length * 2];
+            for (int i = 0; i < this.clientes.length; i++) {
+                arrayDuplicado[i] = this.clientes[i];
+            }
+            this.clientes = arrayDuplicado;
+        }
+    }
 }
--- src/br/ufrpe/Projeto_PetShop/repositorio/RepositorioConsulta.java
+++ src/br/ufrpe/Projeto_PetShop/repositorio/RepositorioConsulta.java
@@ -5,32 +5,40 @@ import br.ufrpe.Projeto_PetShop.repositorio.beans.Consulta;
 public class RepositorioConsulta {
 	private Consulta consultas[]=new Consulta[5];
 	private int consultasTam=0;
-	
-	public RepositorioConsulta() {}
-	
+	//mudar o remover
+	public RepositorioConsulta() {}	
 	private Consulta procurarConsulta (Consulta consulta) {
 		for(int i=0;i<consultasTam;i++) {
 			consultas[i].equals(consulta);
 		}
 		return null;
-	}
-	
+	}	
 	public void adicionarConsulta(Consulta consulta) {
+		if(this.consultasTam == this.consultas.length) {
+			this.duplicaArray();
+		}
 		this.consultas[consultasTam]=consulta;
 		consultasTam++;
-	}
-	
+	}	
 	public Consulta getConsulta(Consulta consulta) {
 		return this.procurarConsulta(consulta);
-	}
-	
+	}	
 	public void removeConsulta(Consulta consulta) {
 		for(int i=0;i<consultasTam;i++) {
 			if(consultas[i].equals(consulta)) {
 				this.consultas[i]=this.consultas[consultasTam-1];
 				this.consultas[consultasTam-1]=null;
-				consultasTam--;
+				this.consultasTam--;
 			}
 		}
-	}
+	}		
+	private void duplicaArray() {
+        if (this.consultas != null && this.consultas.length > 0) {
+            Consulta[] arrayDuplicado = new Consulta[this.consultas.length * 2];
+            for (int i = 0; i < this.consultas.length; i++) {
+                arrayDuplicado[i] = this.consultas[i];
+            }
+            this.consultas = arrayDuplicado;
+        }
+    }
 }
