ccbb70a4771cc148c77af9d5a1c9e70c377f6716
Brenno Barboza <brennol.c.b@hotmail.com>
2018-06-19 13:23:20 -0300

    Pequenas correções

--- src/br/ufrpe/Projeto_PetShop/repositorio/IRepositorioConsulta.java
+++ src/br/ufrpe/Projeto_PetShop/repositorio/IRepositorioConsulta.java
@@ -3,7 +3,7 @@ package br.ufrpe.Projeto_PetShop.repositorio;
 import br.ufrpe.Projeto_PetShop.repositorio.beans.Consulta;
 
 public interface IRepositorioConsulta {
-	public void adicionarConsulta(Consulta consulta);
+	public void addConsulta(Consulta consulta);
 	public Consulta getConsulta(Consulta consulta);
-	public void removeConsulta(Consulta consulta);
+	public void removerConsulta(Consulta consulta);
 }
