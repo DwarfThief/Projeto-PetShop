e26df2cecf653ce04c9bf474def53fb061734a30
Brenno Barboza <brennol.c.b@hotmail.com>
2018-06-21 20:01:49 -0300

    CheckType virou método no CheckLogin

--- src/br/ufrpe/Projeto_PetShop/controller/CheckLogin.java
+++ src/br/ufrpe/Projeto_PetShop/controller/CheckLogin.java
@@ -1,6 +1,10 @@
 package br.ufrpe.Projeto_PetShop.controller;
 
 import br.ufrpe.Projeto_PetShop.repositorio.IRepositorioFuncionario;
+import br.ufrpe.Projeto_PetShop.repositorio.beans.Funcionario;
+import br.ufrpe.Projeto_PetShop.repositorio.beans.Gerente;
+import br.ufrpe.Projeto_PetShop.repositorio.beans.Vendedor;
+import br.ufrpe.Projeto_PetShop.repositorio.beans.Veterinario;
 
 public class CheckLogin {
 	private IRepositorioFuncionario instanceRepFuncionario;
@@ -8,36 +12,32 @@ public class CheckLogin {
 	public CheckLogin(IRepositorioFuncionario instanceRepFuncionario) {
 		this.instanceRepFuncionario = instanceRepFuncionario;
 	}
-	public boolean checagemLogin(String login) {
-		if(login!=null) {
+	public void checagemLogin(String login, String senha) {
+		boolean validando = true;
+		if(login != null && senha != null) {
 			int pos = 0;
 			do {
 				if(instanceRepFuncionario.getFuncionario(pos) != null) {
-					if(instanceRepFuncionario.getFuncionario(pos).getLogin().equals(login)) {
-						return true;
+					if(instanceRepFuncionario.getFuncionario(pos).getLogin().equals(login)
+							&& instanceRepFuncionario.getFuncionario(pos).getSenha().equals(senha)) {
+						checkType(instanceRepFuncionario.getFuncionario(pos));
 					}
 				}else {
-					return false;
+					validando = false;
 				}
 				pos++;
-			}while(1==1);
+			}while(validando == true);
 		}
-		return false;
 	}
-	public boolean checagemSenha(String senha) {
-		if(senha!=null) {
-			int pos = 0;
-			do {
-				if(instanceRepFuncionario.getFuncionario(pos) != null) {
-					if(instanceRepFuncionario.getFuncionario(pos).getSenha().equals(senha)) {
-						return true;
-					}
-				}else {
-					return false;
-				}
-				pos++;
-			}while(1==1);
+	public void checkType(Funcionario funcionario) {
+		if(funcionario != null) {
+			if(funcionario instanceof Gerente) {
+				//TODO chamar o GUI para Gerente
+			}else if(funcionario instanceof Veterinario) {
+				//TODO chamar o GUI para Veterinario
+			}else if(funcionario instanceof Vendedor) {
+				//TODO chamar o GUI para Vendendor
+			}
 		}
-		return false;
 	}
 }
