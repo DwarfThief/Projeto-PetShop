50c6bbee1b3dfdd683b195c6b14b0e1f973bcc5c
Brenno Barboza <brennol.c.b@hotmail.com>
2018-08-02 18:50:56 -0300

    Resolução de uns problemas básicos na classe
    Desisti de usar StringProperty na classe, quando eu tiver mais tempo de estudo talvez eu implemente mais tarde, a ideia parece-me boa. :D

--- src/br/ufrpe/Projeto_PetShop/repositorio/beans/Vendedor.java
+++ src/br/ufrpe/Projeto_PetShop/repositorio/beans/Vendedor.java
@@ -1,14 +1,12 @@
 package br.ufrpe.Projeto_PetShop.repositorio.beans;
 
-import javafx.beans.property.SimpleStringProperty;
-
 public class Vendedor extends Funcionario {
 	
 	public Vendedor(String nome, String cpf, String login, String senha) {
-		super.nome = new SimpleStringProperty((String) nome);
-		super.cpf = new SimpleStringProperty((String) cpf);
-		super.login = new SimpleStringProperty((String) login);
-		super.senha = new SimpleStringProperty((String) senha);
+		super.nome = nome;
+		super.cpf = cpf;
+		super.login = login;
+		super.senha = senha;
 	}
 	@Override
 	public String toString() {
